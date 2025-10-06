8b7a92d913248386e12ad99bd0bb057a78a70010
JefeATX <52305686+JefeATX@users.noreply.github.com>
2025-10-06 16:48:43 -0500

    Refactor insert_iso.sh for better mode handling

--- insert_iso.sh
+++ insert_iso.sh
@@ -1,17 +1,16 @@
-#! /bin/bash -eu
+#!/bin/bash -eu
 
 iso="$1"
 MODE=${2:-cd}
 
-if ! mountpoint -q /iso; then
-    exit 1
-fi
-if ! realpath "$1" |egrep "^/iso/"; then
-    exit 1
-fi
-
-if [ "$MODE" == "cd" ] ; then
-    modprobe g_mass_storage "file=$1" luns=1 stall=0 ro=1 cdrom=1 removable=1
-elif [ "$MODE" == "usb" ] ; then
-    modprobe g_mass_storage "file=$1" luns=1 stall=0 ro=0 cdrom=0 removable=1
+# Only mount as USB mass storage for USB/HDD mode, and ISO for CD-ROM
+if [ "$MODE" == "cd" ]; then
+    # If it's a .cue file, use cdemu, otherwise use g_mass_storage as a CD-ROM
+    if [[ "$iso" == *.cue ]]; then
+        cdemu load 0 "$iso"
+    else
+        modprobe g_mass_storage file="$iso" luns=1 stall=0 ro=1 cdrom=1 removable=1
+    fi
+elif [ "$MODE" == "usb" ]; then
+    modprobe g_mass_storage file="$iso" luns=1 stall=0 ro=0 cdrom=0 removable=1
 fi
