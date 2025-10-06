76ddad2a52de1d2798ce0d4f51c227836d81a75e
JefeATX <52305686+JefeATX@users.noreply.github.com>
2025-10-06 16:18:54 -0500

    Enhance CD mode to handle .cue files
    Added conditional check for .cue files before mounting.

--- mode.sh
+++ mode.sh
@@ -10,7 +10,12 @@ source "$IDIR/clean.sh"
 if [ "$MODE" == "hdd" ] ; then
     modprobe g_mass_storage file=/iso.img luns=1 stall=0 ro=0 cdrom=0 removable=1
 elif [ "$MODE" == "cd" ] ; then
-    mount -o ro "$(losetup -PLf /iso.img --show)p1" /iso
+    # Check for .cue, .iso, or other file type
+    if ls /iso.img 2>/dev/null | grep -q '\.cue$'; then
+        cdemu load 0 /iso.img
+    else
+        mount -o ro,loop /iso.img /iso
+    fi
 elif [ "$MODE" == "usb" ] ; then
     mount "$(losetup -PLf /iso.img --show)p1" /iso
 elif [ "$MODE" == "shutdown" ]; then
